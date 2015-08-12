98edfd422cf306c08d9c3056386af6242f097cb7
Nebucatnetzer <andreas@2li.ch>
2015-08-12 21:29:30 +0200

    adding separate restore script for my laptop

---
+++ restore_laptop.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+stow -t /home/andreas bash
+stow -t /home/andreas filezilla
+stow -t /home/andreas geany
+stow -t /home/andreas i3_laptop
+stow -t /home/andreas ranger
+stow -t /home/andreas remarkable
+stow -t /home/andreas terminator
+stow -t /home/andreas themes
+stow -t /home/andreas thunar
+stow -t /home/andreas vim
+stow -t /home/andreas vlc
+stow -t /home/andreas fonts
+stow -t /home/andreas bin
