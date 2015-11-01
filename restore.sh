4f028b7a9fa1320d83f50768487a5b7fb9b9379d
Andreas Zweili <andreas@2li.ch>
2015-11-01 18:16:29 +0100

    removing ranger from the restore script

--- restore.sh
+++ restore.sh
@@ -5,7 +5,6 @@ stow -t /home/andreas bash
 stow -t /home/andreas filezilla
 stow -t /home/andreas geany
 stow -t /home/andreas i3
-stow -t /home/andreas ranger
 stow -t /home/andreas remarkable
 stow -t /home/andreas terminator
 stow -t /home/andreas themes
