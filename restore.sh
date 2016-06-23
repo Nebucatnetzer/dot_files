b5b67db164e7a623418a4061f55213fe83588e25
Andreas <andreas@2li.ch>
2016-06-23 10:59:00 +0200

    adding the ability to restore the tmux plugins

--- restore.sh
+++ restore.sh
@@ -2,3 +2,8 @@
 
 stow -t /home/andreas bash
 stow -t /home/andreas vim
+stow -t /home/andreas tmux
+mkdir ~/git_repos
+mkdir ~/.tmux/
+git clone https://github.com/tmux-plugins/tpm ~/git_repos/tpm
+ln -s ~/git_repos/tpm ~/.tmux/
